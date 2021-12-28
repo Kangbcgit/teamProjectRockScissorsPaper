5fc7daf357bbd4e6df4d9779ecd632aa9363b31c
Kangbcgit <Kangbcgit@gmail.com>
2021-12-28 18:29:25 +0900

    finished

--- app.js
+++ app.js
@@ -7,86 +7,84 @@ let $playerGame = document.querySelector('.player-game');
 let $computerGame = document.querySelector('.computer-game');
 let $playerScore = document.querySelector('#player-score');
 let $computerScore = document.querySelector('#computer-score');
+let $averaging = document.querySelector('.averaging');
 
 
 
-let score = {
+const score = {
     userScore: 0,
     aiScore: 0,
     playerPick: 0,
     computerPick: 0,
-    money: 0
+    numberOfAttempts: 0,
+    numberOfAttemptsTry: 0
 };
 
 let computerPick = () => {
     let $computerPick = Math.floor(Math.random() * 3) + 1;
     return $computerPick;
 };
+let scoreDataEnter = (playerPick, computerpick) => {
+    score.playerPick = playerPick;
+    score.computerPick = computerpick;
+    if (score.numberOfAttempts >= 10) {
+        score.numberOfAttemptsTry += 1;
+    } else {
+        score.numberOfAttempts += 1;
+    }
+}
 let vs = (e, count) => {
-    let $computerPick = computerPick();
+    let $computerPick = score.computerPick;
+    if (score.numberOfAttempts >= 9) {
+        if (score.numberOfAttempts === 10) {
+            $computerPick = score.computerPick;
+        } else {
+            $averaging.classList.add('active');
+            $averaging.textContent = `운빨 보정 ${3 - score.numberOfAttemptsTry}회`
+        }
+    } else {
+        $computerPick = computerPick();
+    }
+    let victory = [3, 1, 2];
     if ($computerPick === count) {
-        score.playerPick = $computerPick;
-        score.computerPick = $computerPick;
+        scoreDataEnter(count, $computerPick);
         return;
     } else {
-        if ($computerPick === 1) {
-            if (count === 2) {
-                ++score.userScore;
-            }
-            if (count === 3) {
-                ++score.aiScore;
-            }
-        } else if ($computerPick === 2) {
-            if (count === 1) {
-                ++score.aiScore;
-            } else if (count === 3) {
-                ++score.userScore;
-            }
-        } else if ($computerPick === 3) {
-            if (count === 1) {
-                ++score.userScore;
-            } else if (count === 2) {
-                ++score.aiScore;
-            }
+        scoreDataEnter(count, $computerPick);
+        if (victory[count - 1] === $computerPick) {
+            ++score.userScore;
+        } else {
+            ++score.aiScore;
         }
     }
-    score.playerPick = count;
-    score.computerPick = $computerPick;
-
+    if (score.numberOfAttemptsTry >= 3) {
+        $averaging.classList.remove('active');
+        $averaging.textContent = `운빨 보정`;
+        score.numberOfAttemptsTry = 0;
+        score.numberOfAttempts += -10;
+    }
 }
 let changeGameImage = () => {
-    if (score.playerPick === 1) {
-        $playerGame.classList.add('scissors');
-        $playerGame.classList.remove('paper');
-        $playerGame.classList.remove('rock');
-    } else if (score.playerPick === 2) {
-        $playerGame.classList.add('rock');
-        $playerGame.classList.remove('paper');
-        $playerGame.classList.remove('scissors');
-    } else if (score.playerPick === 3) {
-        $playerGame.classList.add('paper');
-        $playerGame.classList.remove('rock');
-        $playerGame.classList.remove('scissors');
-    }
+    let targetClass = ['scissors', 'rock', 'paper'];
+    for (let i = 0, j = targetClass.length; i < j; i++) {
+        if (score.playerPick === i+1) {
+            $playerGame.classList.add(targetClass[i]);
+        } else {
+            $playerGame.classList.remove(targetClass[i])
+        }
 
-    if (score.computerPick === 1) {
-        $computerGame.classList.add('scissors');
-        $computerGame.classList.remove('paper');
-        $computerGame.classList.remove('rock');
-    } else if (score.computerPick === 2) {
-        $computerGame.classList.add('rock');
-        $computerGame.classList.remove('paper');
-        $computerGame.classList.remove('scissors');
-    } else if (score.computerPick === 3){
-        $computerGame.classList.add('paper');
-        $computerGame.classList.remove('rock');
-        $computerGame.classList.remove('scissors');
+        if (score.computerPick === i+1) {
+            $computerGame.classList.add(targetClass[i]);
+        } else {
+            $computerGame.classList.remove(targetClass[i])
+        }
     }
 }
-let  changeScoreboard = () => {
+let changeScoreboard = () => {
     $playerScore.textContent = score.userScore;
     $computerScore.textContent = score.aiScore;
 }
+
 
 //실행
 (() => {
