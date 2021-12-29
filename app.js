a24b56482262005a1b7daa8543be65416fbdab91
Kangbcgit <Kangbcgit@gmail.com>
2021-12-29 14:14:05 +0900

    final

--- app.js
+++ app.js
@@ -1,88 +1,98 @@
-// 함수 생성
-
-let $rock = document.querySelector('#rock');
+// 함수 생성, 전역 변수
+const data = {
+    player: {
+        user: {
+            score: 0,
+            pick: 0,
+        },
+        computer: {
+            score: 0,
+            pick: 0
+        }
+    },
+    round: 0,
+    userChance: 0
+}
+//하단 가위,바위,보 선택창
 let $scissors = document.querySelector('#scissors');
+let $rock = document.querySelector('#rock');
 let $paper = document.querySelector('#paper');
+//유저 선택, 컴퓨터 선택창
 let $playerGame = document.querySelector('.player-game');
 let $computerGame = document.querySelector('.computer-game');
+//유저, 컴퓨터 점수
 let $playerScore = document.querySelector('#player-score');
 let $computerScore = document.querySelector('#computer-score');
+//찬스
 let $averaging = document.querySelector('.averaging');
 
-
-
-const score = {
-    userScore: 0,
-    aiScore: 0,
-    playerPick: 0,
-    computerPick: 0,
-    numberOfAttempts: 0,
-    numberOfAttemptsTry: 0
-};
-
-let computerPick = () => {
-    let $computerPick = Math.floor(Math.random() * 3) + 1;
-    return $computerPick;
-};
-let scoreDataEnter = (playerPick, computerpick) => {
-    score.playerPick = playerPick;
-    score.computerPick = computerpick;
-    if (score.numberOfAttempts >= 10) {
-        score.numberOfAttemptsTry += 1;
-    } else {
-        score.numberOfAttempts += 1;
+//하단에서 선택한 pick 체크 
+let footerPick = e => {
+    let count;
+    if (e.target === $scissors) {
+        count = 1;
+    } else if (e.target === $rock) {
+        count = 2;
+    } else if (e.target === $paper) {
+        count = 3;
     }
+    return count;
 }
-let vs = (e, count) => {
-    let $computerPick = score.computerPick;
-    if (score.numberOfAttempts >= 9) {
-        if (score.numberOfAttempts === 10) {
-            $computerPick = score.computerPick;
-        } else {
-            $averaging.classList.add('active');
-            $averaging.textContent = `운빨 보정 ${3 - score.numberOfAttemptsTry}회`
-        }
-    } else {
-        $computerPick = computerPick();
-    }
+//컴퓨터 가위바위보 랜덤 값
+let computerPick = () => {
+    return Math.floor(Math.random() * 3) + 1;
+}
+//데이터에 유저, 컴퓨터의 선택을 입력
+let scoreInputData = (userPick, computerPick) => {
+    data.player.user.pick = userPick;
+    data.player.computer.pick = computerPick;
+}
+//유저선택 vs 컴퓨터랜덤값 후 데이터 값에 점수 더하기 및 패배시 운빨보정 스택+1
+let vs = (e, userPick) => {
+    let $computerPick = computerPick();
     let victory = [3, 1, 2];
-    if ($computerPick === count) {
-        scoreDataEnter(count, $computerPick);
+    scoreInputData(userPick, $computerPick);
+    if ($computerPick === userPick) {
         return;
     } else {
-        scoreDataEnter(count, $computerPick);
-        if (victory[count - 1] === $computerPick) {
-            ++score.userScore;
+        if (victory[userPick - 1] === $computerPick) {
+            ++data.player.user.score;
         } else {
-            ++score.aiScore;
+            ++data.player.computer.score;
+            data.round += 1;
         }
     }
-    if (score.numberOfAttemptsTry >= 3) {
-        $averaging.classList.remove('active');
-        $averaging.textContent = `운빨 보정`;
-        score.numberOfAttemptsTry = 0;
-        score.numberOfAttempts += -10;
-    }
 }
+//서로의 선택 표시 창
 let changeGameImage = () => {
     let targetClass = ['scissors', 'rock', 'paper'];
     for (let i = 0, j = targetClass.length; i < j; i++) {
-        if (score.playerPick === i+1) {
+        if (data.player.user.pick === i + 1) {
             $playerGame.classList.add(targetClass[i]);
         } else {
-            $playerGame.classList.remove(targetClass[i])
+            $playerGame.classList.remove(targetClass[i]);
         }
 
-        if (score.computerPick === i+1) {
+        if (data.player.computer.pick === i + 1) {
             $computerGame.classList.add(targetClass[i]);
         } else {
-            $computerGame.classList.remove(targetClass[i])
+            $computerGame.classList.remove(targetClass[i]);
         }
     }
 }
+// 스코어 보드에 데이터 반영
 let changeScoreboard = () => {
-    $playerScore.textContent = score.userScore;
-    $computerScore.textContent = score.aiScore;
+    $playerScore.textContent = data.player.user.score;
+    $computerScore.textContent = data.player.computer.score;
+    $averaging.firstElementChild.textContent = data.round;
+}
+//운빨보정 계산
+let vsAveraging = () => {
+    if (data.round >= 10) {
+        data.player.user.score += 3;
+        data.round += -10;
+    }
+    changeScoreboard();
 }
 
 
@@ -91,26 +101,27 @@ let changeScoreboard = () => {
     let $choice = document.querySelector('.choice');
     let $reset = document.querySelector('.reset');
     $choice.addEventListener('click', e => {
-        let count = 0;
         if (!e.target.matches('#rock') && !e.target.matches('#scissors') && !e.target.matches('#paper')) {
             return;
-        } else if (e.target === $scissors) {
-            count = 1;
-        } else if (e.target === $rock) {
-            count = 2;
-        } else if (e.target === $paper) {
-            count = 3;
         }
-        vs(e, count);
+        let userPick = footerPick(e);
+        vs(e, userPick);
         changeGameImage();
         changeScoreboard();
-    })
+    });
     $reset.addEventListener('click', e => {
         if (!e.target.matches('.score-reset-board .reset')) {
             return;
         }
-        score.userScore = 0;
-        score.aiScore = 0;
+        data.player.user.score = 0;
+        data.player.computer.score = 0;
         changeScoreboard();
     });
+    $averaging.addEventListener('click', e => {
+        if (!e.target.matches('.score-reset-board .averaging')) {
+            return;
+        }
+        console.log(data.round);
+        vsAveraging();
+    })
 })();
---
+++ appㅇㅇㅇㅇ.js
@@ -0,0 +1,116 @@
+// 함수 생성
+
+let $rock = document.querySelector('#rock');
+let $scissors = document.querySelector('#scissors');
+let $paper = document.querySelector('#paper');
+let $playerGame = document.querySelector('.player-game');
+let $computerGame = document.querySelector('.computer-game');
+let $playerScore = document.querySelector('#player-score');
+let $computerScore = document.querySelector('#computer-score');
+let $averaging = document.querySelector('.averaging');
+
+
+
+const score = {
+    userScore: 0,
+    aiScore: 0,
+    playerPick: 0,
+    computerPick: 0,
+    numberOfAttempts: 0,
+    numberOfAttemptsTry: 0
+};
+
+let computerPick = () => {
+    let $computerPick = Math.floor(Math.random() * 3) + 1;
+    return $computerPick;
+};
+let scoreDataEnter = (playerPick, computerpick) => {
+    score.playerPick = playerPick;
+    score.computerPick = computerpick;
+    if (score.numberOfAttempts >= 10) {
+        score.numberOfAttemptsTry += 1;
+    } else {
+        score.numberOfAttempts += 1;
+    }
+}
+let vs = (e, count) => {
+    let $computerPick = score.computerPick;
+    if (score.numberOfAttempts >= 9) {
+        if (score.numberOfAttempts === 10) {
+            $computerPick = score.computerPick;
+        } else {
+            $averaging.classList.add('active');
+            $averaging.textContent = `운빨 보정 ${3 - score.numberOfAttemptsTry}회`
+        }
+    } else {
+        $computerPick = computerPick();
+    }
+    let victory = [3, 1, 2];
+    if ($computerPick === count) {
+        scoreDataEnter(count, $computerPick);
+        return;
+    } else {
+        scoreDataEnter(count, $computerPick);
+        if (victory[count - 1] === $computerPick) {
+            ++score.userScore;
+        } else {
+            ++score.aiScore;
+        }
+    }
+    if (score.numberOfAttemptsTry >= 3) {
+        $averaging.classList.remove('active');
+        $averaging.textContent = `운빨 보정`;
+        score.numberOfAttemptsTry = 0;
+        score.numberOfAttempts += -10;
+    }
+}
+let changeGameImage = () => {
+    let targetClass = ['scissors', 'rock', 'paper'];
+    for (let i = 0, j = targetClass.length; i < j; i++) {
+        if (score.playerPick === i+1) {
+            $playerGame.classList.add(targetClass[i]);
+        } else {
+            $playerGame.classList.remove(targetClass[i]);
+        }
+
+        if (score.computerPick === i+1) {
+            $computerGame.classList.add(targetClass[i]);
+        } else {
+            $computerGame.classList.remove(targetClass[i]);
+        }
+    }
+}
+let changeScoreboard = () => {
+    $playerScore.textContent = score.userScore;
+    $computerScore.textContent = score.aiScore;
+}
+
+
+//실행
+(() => {
+    let $choice = document.querySelector('.choice');
+    let $reset = document.querySelector('.reset');
+    $choice.addEventListener('click', e => {
+        let count = 0;
+        if (!e.target.matches('#rock') && !e.target.matches('#scissors') && !e.target.matches('#paper')) {
+            return;
+        } else if (e.target === $scissors) {
+            count = 1;
+        } else if (e.target === $rock) {
+            count = 2;
+        } else if (e.target === $paper) {
+            count = 3;
+        }
+        vs(e, count);
+        changeGameImage();
+        changeScoreboard();
+    })
+    $reset.addEventListener('click', e => {
+        if (!e.target.matches('.score-reset-board .reset')) {
+            return;
+        }
+        score.userScore = 0;
+        score.aiScore = 0;
+        changeScoreboard();
+    });
+})();
